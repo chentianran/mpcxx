f72f66fc3bb71b69f4eac054d8bd3fb40c6b7df3
Tianran Chen <tianran.chen@gmail.com>
2014-04-07 20:43:18 -0400

    Added a few constructors

--- mpcxx.hh
+++ mpcxx.hh
@@ -2,6 +2,7 @@
 #define _MPCXX_HH_
 
 #include <mpc.h>
+#include <complex>
 
 class mpcplx
 {
@@ -13,22 +14,98 @@ public:
     ~mpcplx();
     mpcplx();
 
-private:
+    /// copy constructor ///
+    mpcplx (const mpcplx& z);
 
-    mpc_t z;
+    /// real value constructor ///
+    mpcplx (int         x,		    mp_prec_t prec = mpcplx::get_default_prec(), mpc_rnd_t mode = mpcplx::get_default_rnd());
+    mpcplx (float       x,		    mp_prec_t prec = mpcplx::get_default_prec(), mpc_rnd_t mode = mpcplx::get_default_rnd());
+    mpcplx (double      x,		    mp_prec_t prec = mpcplx::get_default_prec(), mpc_rnd_t mode = mpcplx::get_default_rnd());
+    mpcplx (long double x,		    mp_prec_t prec = mpcplx::get_default_prec(), mpc_rnd_t mode = mpcplx::get_default_rnd());
+    mpcplx (mpf_t       x,		    mp_prec_t prec = mpcplx::get_default_prec(), mpc_rnd_t mode = mpcplx::get_default_rnd());
+
+    /// real and imaginary parts constructor ///
+    mpcplx (int         x, int         y,   mp_prec_t prec = mpcplx::get_default_prec(), mpc_rnd_t mode = mpcplx::get_default_rnd());
+    mpcplx (float       x, float       y,   mp_prec_t prec = mpcplx::get_default_prec(), mpc_rnd_t mode = mpcplx::get_default_rnd());
+    mpcplx (double      x, double      y,   mp_prec_t prec = mpcplx::get_default_prec(), mpc_rnd_t mode = mpcplx::get_default_rnd());
+    mpcplx (long double x, long double y,   mp_prec_t prec = mpcplx::get_default_prec(), mpc_rnd_t mode = mpcplx::get_default_rnd());
+    
+    /// complex value constructor ///
+    mpcplx (std::complex<float>       z,    mp_prec_t prec = mpcplx::get_default_prec(), mpc_rnd_t mode = mpcplx::get_default_rnd());
+    mpcplx (std::complex<double>      z,    mp_prec_t prec = mpcplx::get_default_prec(), mpc_rnd_t mode = mpcplx::get_default_rnd());
+    mpcplx (std::complex<long double> z,    mp_prec_t prec = mpcplx::get_default_prec(), mpc_rnd_t mode = mpcplx::get_default_rnd());
+
+protected:
+
+    mpc_t mpc;
 };
 
-/// Destructor ///
+// Destructor ///
 inline mpcplx::~mpcplx()
 {
-    mpc_clear (z);
+    mpc_clear (mpc);
 }
 
-/// Constructors ///
+// Constructors ////////////////////////////////////////////////////////////////
 inline mpcplx::mpcplx()
 {
-    mpc_init2 (z, get_default_prec());
+    mpc_init2 (mpc, get_default_prec());
 }
+
+inline mpcplx::mpcplx (const mpcplx& z)
+{
+    mpc_init2 (mpc, get_default_prec());
+    mpc_set (mpc, z.mpc, mpcplx::get_default_rnd());
+}
+
+inline mpcplx::mpcplx (int x, mp_prec_t prec, mpc_rnd_t mode)
+{
+    mpc_init2 (mpc, prec);
+    mpc_set_si (mpc, static_cast<long int> (x), mode);
+}
+
+inline mpcplx::mpcplx (float x, mp_prec_t prec, mpc_rnd_t mode)
+{
+    mpc_init2 (mpc, prec);
+    mpc_set_d (mpc, static_cast<double> (x), mode);
+}
+
+inline mpcplx::mpcplx (double x, mp_prec_t prec, mpc_rnd_t mode)
+{
+    mpc_init2 (mpc, prec);
+    mpc_set_d (mpc, x, mode);
+}
+
+inline mpcplx::mpcplx (long double x, mp_prec_t prec, mpc_rnd_t mode)
+{
+    mpc_init2 (mpc, prec);
+    mpc_set_ld (mpc, x, mode);
+}
+
+inline mpcplx::mpcplx (int x, int y, mp_prec_t prec, mpc_rnd_t mode)
+{
+    mpc_init2 (mpc, prec);
+    mpc_set_si_si (mpc, static_cast<long int>(x), static_cast<long int>(y), mode);
+}
+
+inline mpcplx::mpcplx (float x, float y, mp_prec_t prec, mpc_rnd_t mode)
+{
+    mpc_init2 (mpc, prec);
+    mpc_set_d_d (mpc, static_cast<double>(x), static_cast<double>(y), mode);
+}
+
+inline mpcplx::mpcplx (double x, double y, mp_prec_t prec, mpc_rnd_t mode)
+{
+    mpc_init2 (mpc, prec);
+    mpc_set_d_d (mpc, x, y, mode);
+}
+
+inline mpcplx::mpcplx (long double x, long double y, mp_prec_t prec, mpc_rnd_t mode)
+{
+    mpc_init2 (mpc, prec);
+    mpc_set_ld_ld (mpc, x, y, mode);
+}
+
 
 #endif
 
--- test.cc
+++ test.cc
@@ -2,7 +2,15 @@
 
 int main ()
 {
-    mpcplx z;
+    mpcplx z0;
+    mpcplx z1 (z0);
+    mpcplx zi (3);
+    mpcplx zf (1.0f);
+    mpcplx zd (1.0);
+
+    mpcplx zii (3,4);
+    mpcplx zff (1.0f, 2.0f);
+    mpcplx zdd (1.0, 2.0);
 
     return 0;
 }
